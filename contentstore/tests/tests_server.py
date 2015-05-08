71578517c74d297389962803464359b7776b7e50
Mike Jones <mike@westerncapelabs.com>
2015-05-08 15:47:21 +0200

    Move tests to folder

--- contentstore/tests/tests_server.py
+++ contentstore/tests/tests_server.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework.authtoken.models import Token
 
-from .models import Schedule, MessageSet, Message, BinaryContent
+from contentstore.models import Schedule, MessageSet, Message, BinaryContent
 
 
 class APITestCase(TestCase):
